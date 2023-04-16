ad41e98f957a0020b4790dc406ea8da584860125
IbrahimOuhamou <130257576+IbrahimOuhamou@users.noreply.github.com>
2023-04-16 18:15:29 +0000

    Add files via upload

---
+++ imgui_sdl_template/imgui_SDl_includes.h
@@ -0,0 +1,6 @@
+//بسم الله الرحمن الرحيم
+
+#include "third_party/imgui/imgui.h"
+#include "third_party/imgui/imgui_impl_sdl2.h"
+#include "third_party/imgui/imgui_impl_sdlrenderer.h"
+#include <SDL.h>
